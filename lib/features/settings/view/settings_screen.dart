07ecb0493a9983c5a99afcd91f3e758253094f85
efedotov <fedmehol@icloud.com>
2024-09-20 16:26:55 +0300

    update settings ui

--- lib/features/settings/view/settings_screen.dart
+++ lib/features/settings/view/settings_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:the_quotation_book/theme/cubit/theme_cubit.dart';
 
 @RoutePage()
 class SettingsScreen extends StatelessWidget {
@@ -7,8 +9,63 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('settings'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.05),
+          child: Column(
+            children: [
+              BlocBuilder<ThemeCubit, ThemeState>(
+                builder: (context, state) {
+                  return Card(
+                    child: ListTile(
+                      leading: Icon(
+                          state.isDark ? Icons.light_mode : Icons.dark_mode),
+                      title: const Text('Theme'),
+                      trailing: Switch(
+                          value: state.isDark,
+                          onChanged: (value) => _setTheme(context, value)),
+                    ),
+                  );
+                },
+              ),
+              Card(
+                child: ListTile(
+                  leading: const Icon(Icons.notifications_outlined),
+                  title: const Text('Notification'),
+                  onTap: () {},
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: const Icon(Icons.delete,
+                  color: Colors.red,),
+                  title: const Text('Clear Cash'),
+                  onTap: () {},
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: const Icon(Icons.info,
+                  color: Colors.green,),
+                  title: const Text('About'),
+                  onTap: () {},
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  _setTheme(BuildContext context, bool value) {
+    context
+        .read<ThemeCubit>()
+        .setThemeBright(value ? Brightness.dark : Brightness.light);
   }
 }
