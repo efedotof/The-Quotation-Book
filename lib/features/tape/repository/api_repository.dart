c47f200990b9fd476fc76ca746b86a0fed6f3a50
efedotov <fedmehol@icloud.com>
2024-09-29 16:19:23 +0300

    add dio request

--- lib/features/tape/repository/api_repository.dart
+++ lib/features/tape/repository/api_repository.dart
@@ -5,7 +5,7 @@ import '../widget/model/quote.dart';
 
 class ApiRepository {
   final Dio _dio = Dio();
-  final String _baseUrl = 'http://192.168.31.152:3000';
+  final String _baseUrl = '/////';
 
   Future<List<Quote>> fetchAll() async {
     try {
@@ -17,12 +17,11 @@ class ApiRepository {
         throw Exception('Failed to load');
       }
     } catch (e) {
-      debugPrint('fetchAll Error: $e');
       throw Exception('Error occurred: $e');
     }
   }
 
-  Future<List<Quote>> fetchRandom(int count) async {
+  Future<List<Quote>> fetchRandom (int count) async {
     try {
       final response =
           await _dio.get('$_baseUrl/random', queryParameters: {'count': count});
@@ -33,25 +32,31 @@ class ApiRepository {
         throw Exception('Failed to load');
       }
     } catch (e) {
-      debugPrint('fetchRandom Error: $e');
       throw Exception('Error occurred: $e');
     }
   }
 
-  Future<List<Quote>> fetchSearch({String? query, String? category, required List<String> categories}) async {
-    try {
-      final response = await _dio.get('$_baseUrl/search', queryParameters: {
-        if (query != null && query.isNotEmpty) 'q': query,
-        if (category != null && category.isNotEmpty) 'category': category
-      });
-      if (response.statusCode == 200) {
-        List<dynamic> data = response.data;
-        return data.map((item) => Quote.fromJson(item)).toList();
-      } else {
-        throw Exception('Failed to load');
-      }
-    } catch (e) {
-      throw Exception('Error occurred: $e');
+ Future<List<Quote>> fetchSearch(
+    {String? query, List<String>? categories, int limit = 15}) async {
+  try {
+    final queryParams = {
+      if (query != null && query.isNotEmpty) 'q': query,
+      if (categories != null && categories.isNotEmpty)
+        'category': categories.join(','),
+      'limit': limit,
+    };
+
+    final response =
+        await _dio.get('$_baseUrl/search', queryParameters: queryParams);
+    if (response.statusCode == 200) {
+      List<dynamic> data = response.data;
+      return data.map((item) => Quote.fromJson(item)).toList();
+    } else {
+      throw Exception('Failed to load');
     }
+  } catch (e) {
+    debugPrint('fetchSearch Error: $e');
+    throw Exception('Error occurred: $e');
   }
+}
 }
